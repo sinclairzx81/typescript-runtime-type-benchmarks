54b0fd219aed98efdd9c91d068a9f96d2432cc5d
sinclairzx81 <haydn.developer@gmail.com>
2025-01-28 06:33:40 +0900

    Completed Test Run

--- cases/sinclair-typemap-valibot-just-in-time.ts
+++ cases/sinclair-typemap-valibot-just-in-time.ts
@@ -2,33 +2,37 @@ import { object, number, string, boolean, strictObject } from 'valibot';
 import { createCase } from '../benchmarks';
 import { Compile } from '@sinclair/typemap';
 
-const LooseSchema = Compile(object({
-  number: number(),
-  negNumber: number(),
-  maxNumber: number(),
-  string: string(),
-  longString: string(),
-  boolean: boolean(),
-  deeplyNested: object({
-    foo: string(),
-    num: number(),
-    bool: boolean(),
+const LooseSchema = Compile(
+  object({
+    number: number(),
+    negNumber: number(),
+    maxNumber: number(),
+    string: string(),
+    longString: string(),
+    boolean: boolean(),
+    deeplyNested: object({
+      foo: string(),
+      num: number(),
+      bool: boolean(),
+    }),
   }),
-}));
+);
 
-const StrictSchema = Compile(strictObject({
-  number: number(),
-  negNumber: number(),
-  maxNumber: number(),
-  string: string(),
-  longString: string(),
-  boolean: boolean(),
-  deeplyNested: strictObject({
-    foo: string(),
-    num: number(),
-    bool: boolean(),
+const StrictSchema = Compile(
+  strictObject({
+    number: number(),
+    negNumber: number(),
+    maxNumber: number(),
+    string: string(),
+    longString: string(),
+    boolean: boolean(),
+    deeplyNested: strictObject({
+      foo: string(),
+      num: number(),
+      bool: boolean(),
+    }),
   }),
-}));
+);
 
 createCase('@sinclair/typemap-valibot-(just-in-time)', 'assertLoose', () => {
   return data => {
--- cases/sinclair-typemap-zod-just-in-time.ts
+++ cases/sinclair-typemap-zod-just-in-time.ts
@@ -2,37 +2,41 @@ import { Compile } from '@sinclair/typemap';
 import { createCase } from '../benchmarks';
 import * as z from 'zod';
 
-const LooseSchema = Compile(z.object({
-  number: z.number(),
-  negNumber: z.number(),
-  maxNumber: z.number(),
-  string: z.string(),
-  longString: z.string(),
-  boolean: z.boolean(),
-  deeplyNested: z.object({
-    foo: z.string(),
-    num: z.number(),
-    bool: z.boolean(),
-  }),
-}));
-
-const StrictSchema = Compile(z
-  .object({
+const LooseSchema = Compile(
+  z.object({
     number: z.number(),
     negNumber: z.number(),
     maxNumber: z.number(),
     string: z.string(),
     longString: z.string(),
     boolean: z.boolean(),
-    deeplyNested: z
-      .object({
-        foo: z.string(),
-        num: z.number(),
-        bool: z.boolean(),
-      })
-      .strict(),
-  })
-  .strict());
+    deeplyNested: z.object({
+      foo: z.string(),
+      num: z.number(),
+      bool: z.boolean(),
+    }),
+  }),
+);
+
+const StrictSchema = Compile(
+  z
+    .object({
+      number: z.number(),
+      negNumber: z.number(),
+      maxNumber: z.number(),
+      string: z.string(),
+      longString: z.string(),
+      boolean: z.boolean(),
+      deeplyNested: z
+        .object({
+          foo: z.string(),
+          num: z.number(),
+          bool: z.boolean(),
+        })
+        .strict(),
+    })
+    .strict(),
+);
 
 createCase('@sinclair/typemap-zod-(just-in-time)', 'assertLoose', () => {
   return data => {
